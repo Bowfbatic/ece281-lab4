d09f1498b64e0f03a3f3e2de478c8c04546f51ad
Bowfbatic <iamiles629@outlook.com>
2024-04-11 12:59:57 -0600

    finished basic functionality

--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -92,21 +92,69 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
   
 	-- declare components and signals
-
+    component elevator_controller_fsm is
+		Port ( i_clk 	 : in  STD_LOGIC;
+			   i_reset 	 : in  STD_LOGIC; -- synchronous
+			   i_stop 	 : in  STD_LOGIC;
+			   i_up_down : in  STD_LOGIC;
+			   o_floor 	 : out STD_LOGIC_VECTOR (3 downto 0));
+	end component elevator_controller_fsm;
+	
+	component sevenSegDecoder is
+	   Port ( i_D  :   in STD_LOGIC_VECTOR (3 downto 0);
+	          o_S  :   out STD_LOGIC_VECTOR (6 downto 0));
+	end component sevenSegDecoder;
+	
+	component clock_divider is 
+    generic ( constant k_DIV : natural := 2	);
+	port ( 	i_clk    : in std_logic;		   -- basys3 clk
+			i_reset  : in std_logic;		   -- asynchronous
+			o_clk    : out std_logic		   -- divided (slow) clock
+	); 
+	end component clock_divider;
+	
+	-- signals
+	signal w_clk   :   STD_LOGIC;
+	signal w_floor :   STD_LOGIC_VECTOR (3 downto 0);
+	signal w_elevator_reset:   STD_LOGIC;
+	signal w_clk_reset     :   STD_LOGIC;
+	
   
 begin
 	-- PORT MAPS ----------------------------------------
-
-	
+   	elevator_controller_init:   elevator_controller_fsm
+   	port map (
+   	    i_clk => w_clk,
+   	    i_reset => w_elevator_reset,
+   	    i_stop => sw(0),
+   	    i_up_down => sw(1),
+   	    o_floor => w_floor
+   	);
+   	
+   	sevenSegDecoder_init:   sevenSegDecoder
+   	port map (
+   	    i_d => w_floor,
+   	    o_S => seg
+   	);
+   	
+   	clkdiv_inst :   clock_divider
+   	    generic map ( k_DIV => 2 * 25000000 )
+   	    port map (
+   	        i_clk => clk,
+   	        i_reset => w_clk_reset,
+   	        o_clk => w_clk
+   	    );
 	
 	-- CONCURRENT STATEMENTS ----------------------------
-	
+	w_elevator_reset <= btnR or btnU;
+	w_clk_reset <= btnU or btnL;
 	-- LED 15 gets the FSM slow clock signal. The rest are grounded.
-	
-
+	led(15) <= w_clk;
+	led(14 downto 0) <= (others => '0');
 	-- leave unused switches UNCONNECTED. Ignore any warnings this causes.
 	
 	-- wire up active-low 7SD anodes (an) as required
+	an  <= (2 => '0', others => '1');
 	-- Tie any unused anodes to power ('1') to keep them off
 	
 end top_basys3_arch;
